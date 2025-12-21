45e54df4322be677f9c6173c1502d840afb22779
maksimkarbovnik <getuch375@gmail.com>
2025-12-21 13:05:42 +0200

    add function add_prd_card_in_product_datail

--- my_shop/card/urls.py
+++ my_shop/card/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('cart/remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
     path('checkout/', views.checkout, name='checkout'),
     path('payment/<int:order_id>/', views.payment, name='payment'),
-
+    path('card_add_product_detail/', views.card_add_product_detail, name='card_add_product_detail')
 ] 
 
--- my_shop/card/views.py
+++ my_shop/card/views.py
@@ -10,6 +10,7 @@ from card.cart import HybridCart
 from django.db.models import Q
 from payment.utils import get_liqpay_context
 from users.forms import UserRegistrationForm
+import json
 
 
 @require_POST
@@ -20,6 +21,26 @@ def cart_add(request, product_id):
     messages.success(request, "Товар добавлен в корзину!")
     return redirect('product_list')
 
+
+@require_POST
+def card_add_product_detail(request):
+    if request.method == "POST":
+        data_json = json.loads(request.body)
+        product_id = data_json.get("product_id")
+        quantity = data_json.get("quantity")
+
+        #Зробити валідацію
+
+        cart = HybridCart(request)
+        try:
+            product = Product.objects.filter(id=product_id, available=True)
+        except Exception as ex:
+            print(ex)
+        cart.add(product=product, quantity=quantity)
+        messages.success(request, "Товар добавлен в корзину!")
+        return redirect('product_detail')
+    else:
+        return HttpResponse(403, "forbidden")
 
 def cart_remove(request, product_id):
     cart = HybridCart(request)
